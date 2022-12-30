be2988c34b79e13533cd5c935f4fd0aa379345af
som3a404 <121618236+som3a404@users.noreply.github.com>
2022-12-30 02:24:31 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter frist number");
+           int num1 = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("enter second number");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int i; int y;int x = 0;
+            if (num1 >= 1 && num2 > num1)
+            {
+                for (i = 1; i < num2; i++)
+                {
+                    for (y = 1; y < num2; y++)
+                    {
+                        if (i % y == 0)
+                        {
+                            x++;
+
+                        }
+                                                   
+                    }
+                    if (x == 2)
+                    
+                    
+
+                    
+                    {
+                        Console.WriteLine(i);
+                    }
+                    x = 0;
+                    
+            }
+            }
+            else
+                Console.WriteLine("error");
+            Console.ReadKey();
+            
+
+
+        }    
+    }
+}
